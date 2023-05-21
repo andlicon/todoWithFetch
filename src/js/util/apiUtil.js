b4d38bff906ce3b442e3dee8d7580b4ccc60a5a8
andlicon <andresjzabalac@hotmail.com>
2023-05-21 19:12:30 -0400

    ADD: created all api functions whiout implementation

---
+++ src/js/util/apiUtil.js
@@ -0,0 +1,15 @@
+export const getAll = async (source) => {
+
+}
+
+export const createNewTodo = async (source) => {
+
+}
+
+export const updateEntireList = async (source, newList) => {
+
+}
+
+export const deleteItems = async (source) => {
+
+}
